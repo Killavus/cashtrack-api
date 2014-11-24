2e71c42927ad9bf8205f60318b93943628bf96dd
Pawel <3x4to9@gmail.com>
2014-11-24 15:22:03 +0100

    set timezone on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.time_zone = :local
+  config.time_zone = 'Europe/Warsaw'
 end
