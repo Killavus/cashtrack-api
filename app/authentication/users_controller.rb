e5c2dad7c1415a1f9d67963ca0c952be723afd20
TrzyRazyCztery <3x4to9@gmail.com>
2015-01-06 22:33:16 +0100

    link register_user service with user controller

---
+++ app/authentication/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    register_user = RegisterUser.new
+    register_user.(user_params[:email], user_params[:password])
+    head :created
+  rescue RegisterUser::AlreadyRegistered
+    head :unprocessable_entity
+  rescue RegisterUser::ValidationFailed
+    head :unprocessable_entity
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
