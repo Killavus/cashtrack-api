898b0bd8b1122b7bc6a05092cf56becf0c6d5f15
Marcin Grzywaczewski <killavus@gmail.com>
2015-02-18 13:04:33 +0100

    Updated routes of budgets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :budget, only: [:show, :create] do
+  resources :budget, only: [:show, :create, :destroy] do
     resources :payments, only: :create
     resources :shopping, only: :create
   end
