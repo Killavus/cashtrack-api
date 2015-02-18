15c9b5255934d93062a99ebd99ad0d313b38272f
Marcin Grzywaczewski <killavus@gmail.com>
2015-02-18 01:45:07 +0100

    Updated routes to reflect changes to authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
     resources :shopping, only: :create
   end
 
-  resources :authentication, only: [:create, :index]
+  resources :authentication, only: [:create, :index] do
+    collection do
+      post :map_with_user
+    end
+  end
+
   resources :shopping, only: [:create, :destroy]
   resources :sessions, only: [:create]
 
