9889869486208901218b4be6b023241b362f77fa
Marcin Grzywaczewski <killavus@gmail.com>
2015-02-18 12:19:28 +0100

    Definitely fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,15 @@ Rails.application.routes.draw do
     resources :shopping, only: :create
   end
 
-  resources :authentication, only: [:create, :index] do
+  resources :authentication, only: [:create, :index]
+  resources :users, only: [:create]
+
+  resources :shopping, only: [:create, :destroy]
+  resources :sessions, only: [:create] do
     collection do
       post :link_with_user
     end
   end
-
-  resources :users, only: [:create]
-
-  resources :shopping, only: [:create, :destroy]
-  resources :sessions, only: [:create]
 
   get '/budgetoverview' => 'budgets_overview#show_for_user', constraints: UserConstraint
   get '/budgetoverview' => 'budgets_overview#show_for_session', constraints: SessionConstraint
